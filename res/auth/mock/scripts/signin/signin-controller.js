cb75009631ff21cdb57e3f8cbc02c80a52e85336
璇子姐姐520 <381855657@qq.com>
2019-09-26 15:06:43 +0800

    登陆加了一个验证自己数据库的接口

--- res/auth/mock/scripts/signin/signin-controller.js
+++ res/auth/mock/scripts/signin/signin-controller.js
@@ -5,15 +5,27 @@ module.exports = function SignInCtrl($scope, $http) {
   // 登陆请求接口的地方
   $scope.submit = function() {
     var data = {
+      // name: $scope.signin.username.$modelValue
+      // , email: $scope.signin.email.$modelValue
+      id: 123
+      , username: 'xiaohei'
+      , password: 'woshishui'
+    }
+    var data1 = {
       name: $scope.signin.username.$modelValue
       , email: $scope.signin.email.$modelValue
     }
     $scope.invalid = false
-    // $http.post('47.98.142.217:8080/user/login', data)
-    $http.post('/auth/api/v1/mock', data)
+    $http.post('http://47.98.142.217:8080/user/login', data)
+    // $http.post('/auth/api/v1/mock', data)
       .success(function(response) {
-        $scope.error = null
-        location.replace(response.redirect)
+        $http.post('/auth/api/v1/mock', data1)
+          .success(function(response) {
+            $scope.error = null
+            location.replace(response.redirect)
+          })
+        // $scope.error = null
+        // location.replace(response.redirect)
       })
       .error(function(response) {
         switch (response.error) {
