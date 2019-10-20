5d07afcc3bb5da6d4c423a21b3e83036ed7ccfa1
jeremy <15869294164@163.com>
2019-10-20 19:03:03 +0800

    更新定时器

--- res/app/func/funcsConfig/follow/setting/setting-directive.js
+++ res/app/func/funcsConfig/follow/setting/setting-directive.js
@@ -1,3 +1,4 @@
+// const _findKey = require('lodash/findKey')
 module.exports = function FollowSettingDirective($http, $routeParams) {
   return {
     restrict: 'E'
@@ -9,12 +10,42 @@ module.exports = function FollowSettingDirective($http, $routeParams) {
       scope.error = ''
       scope.switchOnText = '开启'
       scope.switchOffText = '关闭'
+      scope.weekday = {
+        momday: {
+        title: '周一',
+        value: 1
+      }, tuesday: {
+        title: '周二',
+        value: 2
+      }, wednesday: {
+        title: '周三',
+        value: 3
+      }, thursday: {
+        title: '周四',
+        value: 4
+      }, friday: {
+        title: '周五',
+        value: 5
+      }, saturday: {
+        title: '周六',
+        value: 6
+      }, sunday: {
+        title: '周日',
+        value: 0
+      }}
       scope.datetimepickerOptions = {
         format: 'HH:mm',
       }
       $http.get('/app/api/v1/ins_account_detail/' + $routeParams.account)
         .then(res => {
-          scope.insAccount = res.data.data
+          let insAccount = res.data.data
+          let selectedWeek = {}
+          insAccount.config.follow.weekday.map(item => {
+            // let day = _findKey(scope.weekday, i => i.value === item)
+            // selectedWeek[day] = true
+          })
+          scope.insAccount = insAccount
+          scope.selectedWeek = selectedWeek
         }).catch(err => {
         let {
           msg
@@ -32,6 +63,22 @@ module.exports = function FollowSettingDirective($http, $routeParams) {
         $http.post('/app/api/v1/update_ins_follow_state', {
           account,
           status
+        })
+      }
+
+      scope.save = () => {
+        let insAccount = JSON.parse(JSON.stringify(scope.insAccount))
+        let weekday = []
+        Object.keys(scope.selectedWeek).map(item => {
+          if (scope.selectedWeek[item]) {
+            weekday.push(scope.weekday[item].value)
+          }
+        })
+        insAccount.config.follow.weekday = weekday
+
+        console.log(insAccount)
+        $http.post('/app/api/v1/ins/update_config', insAccount).then(res => {
+
         })
       }
     }
