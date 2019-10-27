7a31f8e8d9ace9c98cf5eaf89a6dc3c050cc8e54
jeremy <15869294164@163.com>
2019-10-27 21:45:53 +0800

    日志type筛选

--- res/app/func/funcsConfig/logs/logs-directive.js
+++ res/app/func/funcsConfig/logs/logs-directive.js
@@ -6,7 +6,15 @@ module.exports = function FuncLogsDirective($http, $routeParams) {
     }
     , link: function(scope, element) {
       let serial = $routeParams.serial
-      $http.get('/app/api/v1/ins/logs/' + serial).then(res => {
+      let logType = $routeParams.type
+      let type = ''
+      switch (logType) {
+        case 'autoFollow': type = 1
+          break
+        case 'autoUnfollow': type = 2
+          break
+      }
+      $http.get('/app/api/v1/ins/logs/' + serial + '?type=' + type).then(res => {
         let list = res.data.data
         list.map(item => {
           item.created = window.moment(item.created).format('YYYY-MM-DD HH:mm')
