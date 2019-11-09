dc37014b90c1c8d33b674469a31014efa02a692d
jeremy <15869294164@163.com>
2019-11-09 23:10:32 +0800

    关注资源设置

--- res/app/func/funcsConfig/follow/resource-setting/resource-setting-directive.js
+++ res/app/func/funcsConfig/follow/resource-setting/resource-setting-directive.js
@@ -5,13 +5,13 @@ module.exports = function ResourceSettingDirective($http, $routeParams) {
     , scope: {
     }
     , link: function(scope, element) {
-      scope.users = ''
+      scope.users1 = ''
+      scope.users2 = ''
+      scope.users3 = ''
       scope.type = 1
       $http.get('/app/api/v1/ins_account_detail/' + $routeParams.account)
         .then(res => {
           let insAccount = res.data.data
-          insAccount.config.follow.insUsers = Array.isArray(insAccount.config.follow.insUsers) ?
-            insAccount.config.follow.insUsers : []
           scope.insAccount = insAccount
         }).catch(err => {
         let {
@@ -25,25 +25,42 @@ module.exports = function ResourceSettingDirective($http, $routeParams) {
       /**
        * 添加资源
        */
-      scope.addResource = function() {
+      scope.addResource = function(type) {
+        let resourceType = ''
+        let usersType = ''
+        switch(Number(type)) {
+          case 1: resourceType = 'resource1'
+            usersType = 'users1'
+            break
+          case 2:
+          case 3: resourceType = 'resource2'
+            usersType = 'users2'
+            break
+          case 4:
+          case 5: resourceType = 'resource3'
+            usersType = 'users3'
+            break
+        }
         // 判断users是否有内容
-        if (scope.users) {
-          let users = scope.users.split('\n')
-          scope.users = ''
-          let type = scope.type
+        if (scope[usersType]) {
+          let users = scope[usersType].split('\n')
+          scope[usersType] = ''
 
-          users.map(user => {
-            scope.insAccount.config.follow.insUsers.push({
-              res: user,
-              status: 1,
-              level: 1,
-              type,
-              record: 0,
-              created: window.moment().format('YYYY-MM-DD HH:mm')
+          if (resourceType) {
+            console.log(scope.insAccount.config.follow.insUsers, resourceType)
+            users.map(user => {
+              scope.insAccount.config.follow.insUsers[resourceType].res.push({
+                res: user,
+                status: 1,
+                level: 1,
+                type,
+                record: 0,
+                created: window.moment().format('YYYY-MM-DD HH:mm')
+              })
             })
-          })
 
-          $http.post('/app/api/v1/ins/update_config', scope.insAccount)
+            $http.post('/app/api/v1/ins/update_config', scope.insAccount)
+          }
         }
       }
 
