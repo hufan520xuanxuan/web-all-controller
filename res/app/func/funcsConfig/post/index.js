638bba7e68ccc2d2134d16f8ace2bd851e10734b
璇子姐姐520 <381855657@qq.com>
2019-11-12 22:46:32 +0800

    增加了三个功能的新页面

--- res/app/func/funcsConfig/post/index.js
+++ res/app/func/funcsConfig/post/index.js
@@ -3,7 +3,5 @@ require('./post.css')
 module.exports = angular.module('ccp.func-autopost', [
   require('stf/common-ui').name,
   require('stf/admin-mode').name,
-  // require('./setting').name
-  require('./nowpost').name
 ])
   .directive('autoPost', require('./post-directive'))
