1643d005191e57f6f7ee797062631d8e5c4b2442
璇子姐姐520 <381855657@qq.com>
2019-11-13 11:04:20 +0800

    改了名称

--- res/app/func/funcsConfig/comment/resource-setting/index.js
+++ res/app/func/funcsConfig/comment/resource-setting/index.js
@@ -4,4 +4,4 @@ module.exports = angular.module('ccp.func-comment-resource-setting', [
   require('stf/common-ui').name,
   require('stf/admin-mode').name
 ])
-  .directive('commentResSetting', require('./resource-setting-directive'))
+  .directive('commentResSetting', require('./comment-res-setting-directive'))
