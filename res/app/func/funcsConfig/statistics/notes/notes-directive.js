03c8d2c92d8793f5c9078f3cda76f696e092afda
jeremy <15869294164@163.com>
2019-12-26 13:56:31 +0800

    账号备注

--- res/app/func/funcsConfig/statistics/notes/notes-directive.js
+++ res/app/func/funcsConfig/statistics/notes/notes-directive.js
@@ -1,6 +1,6 @@
 const E = require('wangeditor')
 
-module.exports = function AutoUnFollowDirective($http, $uibModal) {
+module.exports = function AutoUnFollowDirective($http, $routeParams) {
   return {
     restrict: 'E'
     , template: require('./notes.pug')
@@ -9,6 +9,19 @@ module.exports = function AutoUnFollowDirective($http, $uibModal) {
     , link: function(scope, element) {
       let editor = new E('#editor')
       editor.create()
+
+      $http.post('/app/api/v1/ins/get_notes', {
+        account: $routeParams.account
+      }).then(res => {
+        editor.txt.html(res.data.data)
+      })
+
+      scope.save = function() {
+        $http.post('/app/api/v1/ins/save_notes', {
+          notes: editor.txt.html(),
+          account: $routeParams.account
+        })
+      }
     }
   }
 }
