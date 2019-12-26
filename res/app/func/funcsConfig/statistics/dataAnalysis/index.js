d0c09041c68f8852749b22ad0999b04266b74295
jeremy <15869294164@163.com>
2019-12-26 15:22:48 +0800

    echart集成

---
+++ res/app/func/funcsConfig/statistics/dataAnalysis/index.js
@@ -0,0 +1,5 @@
+require('./dataAnalysis.css')
+
+module.exports = angular.module('ccp.func-statistics-data-analysis', [
+])
+  .directive('statisticsDataAnalysis', require('./data-analysis-directive'))
